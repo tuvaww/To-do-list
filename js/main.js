a537b1974d2dbeef6c9eef2524c0e01d56d73a41
tuvaww <tuvali.wahlin@icloud.com>
2021-11-04 21:25:16 +0100

    börjat med js för karusell

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-window.onload = function () {};
+let ul = document.querySelector(".carousel-items");
+let slides = Array.from(ul.children);
+
+let slideWidth = slides[0].getBoundingClientRect().width;
+let nextButton = document.querySelector(".btn-right");
+let prevButton = document.querySelector(".btn-left");
+let navDots = document.querySelector(".carousel-nav");
+let dots = Array.from(navDots.children);
+
+//positionera slides bredvid istället för på varandra
+
+let slidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+
+slides.forEach(slidePosition);
+
+// klicka vänster, flyttar slides till vänster
+// klicka höger, flyttar slides till höger
+
+//flytta slide
+
+nextButton.addEventListener("click", (e) => {
+  let currentSlide = ul.querySelector(".current-slide");
+  let nextSlide = currentSlide.nextElementSibling;
+  let moves = nextSlide.style.left;
+
+  ul.style.transform = "translateX(-" + moves + ")";
+  currentSlide.classList.remove("current-slide");
+  nextSlide.classList.add("current-slide");
+});
+
+//});
+// klicka på nav prickar, flyttar till vald slide
